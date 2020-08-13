ff84d2c347f4dcb0d56a800933995088e0b0867f
Marimuthu PERIYANNAN <marimuthu.periyannan@capgemini.com>
2020-08-13 17:33:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+Vue.component("click-counter", {
+  template: '<button @click="count++"> {{ count }} </button>',
+
+  data() {
+    return {
+      count: 0,
+    };
+  },
+});
+
+new Vue({
+  el: "#app",
+});
